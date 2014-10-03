59a0e0995cf3dc9236ccaa2ea26c4248b09266f5
Gianluca Arbezzano <gianarb92@gmail.com>
2014-10-03 17:32:39 +0200

    Refactoring and grunt tool

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function ( grunt ) {
+  grunt.loadNpmTasks('grunt-aws');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  var taskConfig = {
+    conf: require( './configuration.json' ),
+
+    copy: {
+      v2013: {
+        files: [
+          {
+            src: [ './2013/**' ],
+            dest: 'build/',
+            cwd: '.',
+            expand: true
+          }
+       ]
+      },
+      workshop: {
+        files: [
+          {
+            src: [ './workshop/**' ],
+            dest: 'build/',
+            cwd: '.',
+            expand: true
+          }
+       ]
+      },
+      src: {
+        files: [
+          {
+            src: ['./**'],
+            dest: 'build/',
+            cwd: './src',
+            expand: true
+          }
+       ]
+      }
+    },
+
+    /**
+     * Deploy build dir in S3
+     */
+    s3: {
+      options: {
+        accessKeyId: "<%= conf.aws.accessKeyId %>",
+        secretAccessKey: "<%= conf.aws.secretAccessKey %>",
+        cache: false,
+        access: "public-read",
+        bucket: "<%= conf.aws.bucket %>"
+      },
+      build: {
+        cwd: "./build/",
+        src: "**"
+      }
+    }
+
+  };
+
+  grunt.initConfig(taskConfig);
+
+
+};
